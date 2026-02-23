1b66ee30a7891e49c51aa6c7510728c8f8c934eb
Ali Çınar Taşal <a.cinartasal04@gmail.com>
2026-02-23 17:34:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,33 @@
 import { db } from "./firebase.js";
-import { collection, addDoc, getDocs, onSnapshot } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { collection, addDoc, getDocs, query, where, onSnapshot } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 let currentRoom=null;
 let currentUser=localStorage.nickname;
+let currentUserNo=null;
 
+init();
+
+async function init(){
+const q=query(collection(db,"users"),where("nickname","==",currentUser));
+const snap=await getDocs(q);
+snap.forEach(doc=>{
+currentUserNo=doc.data().userNo;
+});
 loadGroups();
+}
 
 async function loadGroups(){
 const snap=await getDocs(collection(db,"groups"));
+document.getElementById("groupList").innerHTML="";
 snap.forEach(doc=>{
 const data=doc.data();
+if(data.members.includes(currentUserNo)){
 const div=document.createElement("div");
-div.innerText=data.name+" ("+data.code+")";
+div.innerText=data.name;
 div.onclick=()=>selectGroup(data.code,data.name);
 document.getElementById("groupList").appendChild(div);
-});
 }
-
-window.createGroup=async function(){
-const name=prompt("Grup adı?");
-const code=Math.floor(100000+Math.random()*900000).toString();
-await addDoc(collection(db,"groups"),{
-name:name,
-code:code
 });
-alert("Grup kodu: "+code);
-loadGroups();
-}
-
-window.joinGroup=function(){
-alert("Gruba katılmak için kodu bilmen yeterli. Listede varsa tıkla.");
 }
 
 window.selectGroup=function(code,name){
@@ -54,7 +52,7 @@ document.getElementById("chat").appendChild(div);
 }
 
 window.send=async function(){
-if(!currentRoom)return alert("Önce grup seç");
+if(!currentRoom)return alert("Grup seç");
 const msg=document.getElementById("msg").value;
 await addDoc(collection(db,"messages"),{
 room:currentRoom,
