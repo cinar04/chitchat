b52845bdef840821cf1e3ba6a7c87a8207cbc7a7
Ali Çınar Taşal <a.cinartasal04@gmail.com>
2026-02-24 17:25:30 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "APIKEY",
-  authDomain: "PROJECT.firebaseapp.com",
-  projectId: "PROJECT",
-  storageBucket: "PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyCn8TB2KdUoOB8ePfE9oLcic54ZkhSRfIM",
+  authDomain: "chit-f4d4e.firebaseapp.com",
+  projectId: "chit-f4d4e",
+  storageBucket: "chit-f4d4e.firebasestorage.app",
+  messagingSenderId: "1090199679595",
+  appId: "1:1090199679595:web:8ea09750b5b40c8ec6b72e",
+  measurementId: "G-KCH3VD0JZG"
 };
 
 const app = initializeApp(firebaseConfig);
